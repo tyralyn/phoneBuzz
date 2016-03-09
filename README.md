9ebab4248b3e5b6345fc434e35f917a206f52d0d
tyralyn <tyralyntran@gmail.com>
2016-03-08 17:20:36 -0800

    added to do list for part 2 before submission

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # phoneBuzz
+
+TODO:
+
+part 1:
+	* comments
+	* make fizzbuzz voice stuff sound better?
+
+part 2:
+	* input check: check input to make sure it's a valid phone number
+	* remove response in call function
+	* rename "hello-monkey" to something more descriptive
+	* remove call printing
+	* remove return value
+	* in hello-monkey, handle case of non-number input
+
+
--- simplePhoneBuzz.py
+++ simplePhoneBuzz.py
@@ -10,24 +10,27 @@ auth_token = credentials.authToken
 client = TwilioRestClient(account_sid, auth_token)
 app = Flask(__name__)
 
+#entry point for application, renders /templates/index.html for input box
 @app.route('/')
 def start_here():
 	return render_template('index.html')
 
+#obtains input (phone number) from input box, redirects to function that makes call
 @app.route("/get-phone-number", methods=['GET', 'POST']) 
 def get_phone_number():
 	phone_num = request.form['number']
 	return redirect("/call")
 
+#sets up call, moves to
 @app.route("/call", methods=['GET', 'POST']) 
 def call():
 	resp = twilio.twiml.Response()
 	call = client.calls.create(to="+16508621107",  from_= "+16506035470", url="http://705be79e.ngrok.io/"+"hello-monkey")
-
 	for c in client.calls.list():
 		print ("From: " + c.from_formatted + " To: " + c.to_formatted)
 	return str(resp)
 
+#recieves call made by call function, prompts user for input
 @app.route("/hello-monkey", methods=['GET', 'POST'])
 def hello_monkey():
 	resp = twilio.twiml.Response()
@@ -35,6 +38,7 @@ def hello_monkey():
 		inp.say("Please enter a number and end with #")
 	return str(resp)
 
+#takes input from user and goes through fizzbuzz process
 @app.route("/handle-key", methods=['GET', 'POST'])
 def handle_key():
 	digit_pressed = request.values.get('Digits', None)
